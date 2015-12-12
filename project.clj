de51dcb71223b4597eb5efe030809ecac29b6b34
Ryo Fukumuro <rkworks@o0o.co>
2015-12-12 18:10:25 +0900

    Try Codecov instead of Coveralls

--- project.clj
+++ project.clj
@@ -2,23 +2,29 @@
 (def meta-modules ["devel"])
 (def non-meta-modules (remove (set meta-modules) modules))
 
+(defn subdir [path]
+  (mapv #(str % "/" path) modules))
+
 (defproject zou "0.1.0-SNAPSHOT"
   :dependencies [[zou/common :version]
                  [zou/framework :version]
                  [zou/web :version]
                  [zou/db :version]]
   :plugins [[lein-modules "0.3.11"]]
-  :profiles {:coverage {:plugins      [[lein-cloverage "1.0.6"]
-                                       [lein-exec "0.3.5"]
-                                       [cloverage "1.0.6" :exclusions [org.clojure/tools.cli]]
+  :profiles {:coverage {:source-paths ~(subdir "src")
+                        :test-paths ~(subdir "test")
+                        :resource-paths ~(into (subdir "dev-resources")
+                                               (subdir "resources"))
+                        :plugins      [[lein-cloverage "1.0.7-SNAPSHOT"]]
+                        :dependencies [[zou/devel :version]
                                        [org.clojure/tools.cli "0.3.3"]
-                                       [lein-shell "0.4.2"]]
-                        :dependencies [[cheshire "5.5.0"]]}
+                                       [cloverage "1.0.7-SNAPSHOT" :exclusions [org.clojure/tools.cli]]]}
              :deploy {:deploy-repositories [["zou-repo" {:url        "s3p://zou-repo/"
                                                          :username   :env/aws_access_key_id
                                                          :passphrase :env/aws_secret_access_key}]]
                       :plugins [[s3-wagon-private "1.2.0"]]}
-             :dev {:dependencies [[midje "1.8.2"]
+             :dev {:dependencies [[org.clojure/clojurescript "1.7.170"]
+                                  [midje "1.8.2"]
                                   [org.clojure/clojure "1.7.0"]
                                   [clj-http "2.0.0"]
                                   [ring/ring-mock "0.3.0"]
@@ -31,19 +37,7 @@
                                   [selmer "0.9.5"]]
                    :plugins      [[lein-midje "3.2"]]
                    :env          {:zou-env :dev}
-                   :aliases      {"coverage-sub" ["do"
-                                                  ["cloverage" "--coveralls"]
-                                                  ["exec" "-p" "../etc/coverage.clj"]]
-                                  "coverage-all" ~(into ["exec" "-p" "etc/coverage_all.clj"] non-meta-modules)
-                                  "coverage"     ["with-profile" "+coverage"
-                                                  "do"
-                                                  ["modules" ":dirs" ~(clojure.string/join ":" non-meta-modules)
-                                                   "coverage-sub"]
-                                                  ["coverage-all"]]
-                                  "coveralls"    ["with-profile" "+coverage"
-                                                  "shell" "curl" "-F"
-                                                  "json_file=@coveralls.json"
-                                                  "https://coveralls.io/api/v1/jobs"]}}}
+                   :aliases      {"coverage" ["with-profile" "+coverage" "cloverage" "--codecov"]}}}
   :modules {:dirs ~modules
             :subprocess nil
             :inherited
