5c38fd2033597f4db9a8d62c7ef7361218601452
Ryo Fukumuro <rfkm@nop.sh>
2016-03-13 02:06:03 +0900

    Restructure example project

--- example/project.clj
+++ example/project.clj
@@ -3,31 +3,28 @@
 (defproject zou-todo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :min-lein-version "2.5.2"
-  :clean-targets ^{:protect false} [:target-path "resources/public/js/dist" "out"]
-  :repositories [["zou-repo"
-                  {:url "https://s3.amazonaws.com/zou-repo"}]]
+  :repositories [["zou-repo" "https://s3.amazonaws.com/zou-repo"]]
   :uberjar-name "zou-todo.jar"
-  :repl-options {:init-ns zou.framework.repl
-                 :init    (zou.framework.core/boot-core!)}
+  :repl-options {:init-ns zou.framework.repl}
   :source-paths ["src" "src-cljs"]
+  :resource-paths ["resources" "target/sassc" "target/cljs"]
+  :target-path "target/%s"
+  :exclusions   [org.clojure/clojurescript]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [zou ~zou-version]
                  [com.h2database/h2 "1.4.191"]
                  [org.clojure/java.jdbc "0.4.2"]
-                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
                  [stencil "0.5.0"]
                  [selmer "1.0.0"]
                  [hiccup "1.0.5"]]
-  :plugins [[lein-environ "1.0.2"]
-            [rkworks/lein-baum "0.1.0"]
-            [lein-cljsbuild "1.1.2"]]
-  :cljsbuild {import "resources/zou/config/cljsbuild.edn"}
-  :profiles {:uberjar {:env   {:prod true}
-                       :aot   [zou.framework.main #".*"]
-                       :hooks [leiningen.cljsbuild]}
-             :dev     {:env          {:zou-env "dev"}
-                       :dependencies [[zou/devel ~zou-version]
-                                      [midje "1.8.3"]
-                                      [com.cemerick/piggieback "0.2.1"]]
-                       :plugins      [[lein-midje "3.2"]]}}
+  :profiles {:provided {:dependencies [[org.clojure/clojurescript "1.7.228"]
+                                       [zou/devel ~zou-version]]}
+             :repl     {:dependencies [[com.cemerick/piggieback "0.2.1"]]}
+             :uberjar  {:prep-tasks ["javac" "compile"
+                                     ["run" "sassc" "compile"]
+                                     ["run" "cljs" "compile"]]
+                        :aot        [zou.framework.main #".*"]}
+             :dev      {:jvm-opts     ["-Dzou-env=dev"]
+                        :dependencies [[midje "1.8.3"]]
+                        :plugins      [[lein-midje "3.2"]]}}
   :main zou.framework.main)
